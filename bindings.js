125085e6884f3926fb7f61cb9ce0a6c9c7cd4958
Benjamin Landers <benjaminrlanders@gmail.com>
2013-08-29 15:43:51 -0700

    added testing and fixed binding problems

--- bindings.js
+++ bindings.js
@@ -12,11 +12,15 @@ Platformer.setBlockKeyUp = function(func){
 }
 Platformer.bindingsDown = [];
 Platformer.bindingsUp = [];
+Platformer.keysDownBuffer = [];
+Platformer.keysUpBuffer = [];
+
+Platformer.keys = [];
 Platformer.setBindingDown= function(key, func){
-	bindingsDown[key]=func;
+	Platformer.bindingsDown[key]=func;
 }
 Platformer.setBindingUp= function(key, func){
-	bindingsDown[key]=func;
+	Platformer.bindingsDown[key]=func;
 }
 Platformer.init_keys = function(){
 	Platformer.blockKeyUp = function(){
@@ -32,21 +36,21 @@ Platformer.init_keys = function(){
 		return false;
 	}
 	document.onkeydown = function(evt){
-	  if(!keys[evt.keyCode]){
-	    keys[evt.keyCode] = true;
+	  if(!Platformer.keys[evt.keyCode]){
+	    Platformer.keys[evt.keyCode] = true;
 	    console.log(evt.keyCode);
-	    if(!Platformer.BlockKeyDown(evt) || Platformer.UnlockKeyDown(evt))
-	    	if(bindingsDown[evt.keyCode])
-	      		bindingsDown[evt.keyCode]();
+	    if(!Platformer.blockKeyDown(evt) || Platformer.unlockKeyDown(evt))
+	    	if(Platformer.bindingsDown[evt.keyCode])
+	      		Platformer.bindingsDown[evt.keyCode]();
 	  	else
 	  		Platformer.keysDownBuffer.push(evt.keyCode);
 	  }
 	}
 	document.onkeyup = function(evt){
-	  keys[evt.keyCode] = false;
-	  if(!Platformer.BlockKeysUp(evt) || Platformer.UnlockKeyUp(evt)){
-		  if(bindingsUp[evt.keyCode])
-		      bindingsUp[evt.keyCode]();
+	  Platformer.keys[evt.keyCode] = false;
+	  if(!Platformer.blockKeyUp(evt) || Platformer.unlockKeyUp(evt)){
+		  if(Platformer.bindingsUp[evt.keyCode])
+		      Platformer.bindingsUp[evt.keyCode]();
 		}else{
 			Platformer.keysUpBuffer.push(evt.keyCode);
 		}
