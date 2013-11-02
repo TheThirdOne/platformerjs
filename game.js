96d7065c0b6bf843fd5c539347245c66dc4fb45e
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-01 22:23:45 -0700

    added prototype for entity

--- game.js
+++ game.js
@@ -1,23 +1,10 @@
-PLatformer.Entity = function(sprite){
-	this.sprite = new Kinect.Sprite(sprite);
-	this.velocityX = 0;
-	this.velocityY = 0;
-	this.getWidth = function(x){
-		return this.sprite.getWitdh();
-	}
-	this.setX = function (x){
-		this.sprite.setX(x);
-	};
-	this.getX = function (){
-		this.sprite.getX();
-	}
-	this.setScaleX = function(x){
-		this.sprite.setScaleX(x);
-	};
-	this.getScaleX = function(x){
-		this.sprite.getScaleX(x);
-	};
-	this.setDirection = function(direction){
+Platformer.Entity = function(sprite){
+	this.sprite = new Kinetic.Sprite(sprite);
+};
+Platformer.Entity.prototype={
+	dy: 0,
+	dx: 0,
+	setDirection: function(direction){
 		if(direction > 0){
 			if(this.getScaleX() < 0){
 	      		this.setScaleX(1);
@@ -29,5 +16,20 @@ PLatformer.Entity = function(sprite){
 		      this.setX(this.sprite.getX() + this.getWidth()/2);
 		    }
 	    }
-	};
-};
+	},
+	getScaleX: function(){
+		this.sprite.getScaleX();
+	},
+	setScaleX: function(x){
+		this.sprite.setScaleX(x);
+	},
+	getWidth: function(x){
+		return this.sprite.getWitdh();
+	},
+	setX: function (x){
+		this.sprite.setX(x);
+	},
+	getX: function (){
+		this.sprite.getX();
+	}
+}
