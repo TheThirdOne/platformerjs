a2c65c5902b768ab8b6f2ac043c54f594049a777
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-10 12:00:48 -0800

    moved level code

--- examples/test/game.js
+++ examples/test/game.js
@@ -39,7 +39,7 @@ Platformer.onLoad = function(){
         fillPatternImage: Platformer.images[3]
       });
   var layer = new Kinetic.Layer();
-  var level = new levels.base();
+  var level = new Platformer.levelBase();
   layer.add(back);
   Platformer.init('container',600,600);
   Platformer.stage.add(layer);
@@ -49,87 +49,3 @@ Platformer.onLoad = function(){
 }
 Platformer.loadImages(['res/spy.png','res/thug.png','res/ground.png','res/concrete.png',
 	'res/ladder.png','res/knife.png','res/crate.png','res/clock.png','res/alarm.png']);
-var levels = [];
-levels.base = function(){
-    this.clock = new Kinetic.Sprite({
-      x: 904,
-      y: 32,
-      image: Platformer.images[7],
-      animation: 'run',
-      animations: clockanimation,
-      frameRate: .88,
-      index: 0,
-      width: 64,
-      height:64
-    });
-    this.alarm = new Kinetic.Sprite({
-      x: 808,
-      y: 32,
-      image: Platformer.images[8],
-      animation: 'idle',
-      animations: alarmanimation,
-      frameRate: 8,
-      index: 0,
-      width: 64,
-      height:64
-    });
-    this.pauseText = new Kinetic.Text({
-          x: 0,
-          y: 60,
-          text: 'Pause',
-          fontSize: 200,
-          fontFamily: 'Calibri',
-          fill: '#FFF',
-          width: 1000,
-          padding: 20,
-          align: 'center',
-          visible: false
-    });
-    this.gameOver = new Kinetic.Text({
-          x: 0,
-          y: 300,
-          text: 'Press any key to continue',
-          fontSize: 60,
-          fontFamily: 'Calibri',
-          fill: '#FFF',
-          width: 1000,
-          padding: 20,
-          align: 'center',
-          visible: false
-    });
-    this.env = {
-      goingLeft: false,
-      goingRight: false,
-      jumped: false,
-      climb: false,
-      fall: false,
-      cloaked: 0,
-      paused: false,
-      alarms: 3
-    };
-    this.knife = new Kinetic.Sprite({
-      x: 0,
-      y: 0,
-      image: Platformer.images[5],
-      animation: 'idle',
-      animations: knifeanimation,
-      frameRate: 8,
-      index: 0,
-      width: 32,
-      height:64
-    });
-    this.resetCloak = function(){
-      this.env.cloaked = 0;
-      this.clock.setAnimation('idle');
-    };
-    this.cloak = function(){
-      this.env.cloaked++;
-      if(this.clock.getAnimation() != 'run')
-        this.clock.setAnimation('run');
-    };
-    this.win = function(){
-      this.pauseText.setText('You Win');
-      this.gameOver.setVisible(true);
-      bindingsDown[pause]();
-    };
-  }
