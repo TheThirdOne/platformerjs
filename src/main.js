8bdc133e8357bd59a5d600993a7aca109f163c47
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-22 15:32:07 -0800

    cleaned up code

--- src/main.js
+++ src/main.js
@@ -3,24 +3,24 @@ Platformer.playLevel = function(level){
 	Platformer.reset(Platformer.currentLevel);
 	Platformer.currentLevel = level;
 	Platformer.currentLevel.start();
-}
+};
 Platformer.load = function()
 {
 	Platformer.itemsToLoad--;
 	if(Platformer.itemsToLoad === 0){
 		Platformer.onLoad();
 	}
-}
+};
 Platformer.init=function(container,w,h)
 {
 	Platformer.stage = new Kinetic.Stage({
-	  container: container,
-	  width: w,
-	  height: h
+    container: container,
+    width: w,
+    height: h
 	});
-}
+};
 Platformer.reset = function(level){
 	if(!level)
 		return;
 	level.destroy();
-}
+};
--- src/resource.js
+++ src/resource.js
@@ -4,7 +4,7 @@ Platformer.AnimationSet = function(data){
     out[data.anims[i].name]=Platformer.Animation(data.anims[i].slides,data);
   }
   return out;
-}
+};
 Platformer.Animation = function(slides, data){
   var x,y;
   var out=[];
@@ -12,34 +12,34 @@ Platformer.Animation = function(slides, data){
     x = slides[i]%data.columns;
     y = Math.floor(slides[i]/data.columns);
     out[i] = {'x': x*data.width, 'y' : y*data.height,
-           'width': data.width, 'height': data.height}
+           'width': data.width, 'height': data.height};
   }
   return out;
-}
+};
 Platformer.init_sound = function(type, channels, volume){
-  sounds[type]=[]
+  sounds[type]=[];
   for (a=0;a<channels;a++) {                  
     sounds[type][a] = {};
-    sounds[type][a]['channel'] = new Audio();   
-    sounds[type][a]['channel'].src = document.getElementById(type).src; 
-    sounds[type][a]['channel'].load();      
-    sounds[type][a]['finished'] = -1;
+    sounds[type][a].channel = new Audio();   
+    sounds[type][a].channel.src = document.getElementById(type).src; 
+    sounds[type][a].channel.load();      
+    sounds[type][a].finished = -1;
     if(volume)
-      sounds[type][a]['channel'].volume = volume;         
+      sounds[type][a].channel.volume = volume;         
   }
-}
+};
 Platformer.play_multi_sound = function(s, start) {
   for (a=0;a<sounds[s].length;a++) {
     thistime = new Date();
-    temp = sounds[s]
-    if (sounds[s][a]['finished'] < thistime.getTime()) {      
-      sounds[s][a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000 + start*1000;
-      sounds[s][a]['channel'].currentTime = start;
-      sounds[s][a]['channel'].play();
+    temp = sounds[s];
+    if (sounds[s][a].finished < thistime.getTime()) {      
+      sounds[s][a].finished = thistime.getTime() + document.getElementById(s).duration*1000 + start*1000;
+      sounds[s][a].channel.currentTime = start;
+      sounds[s][a].channel.play();
       break;
     }
   }
-}
+};
 Platformer.loadImages = function(src){
   Platformer.images = [];
   Platformer.itemsToLoad = Platformer.itemsToLoad || 0;
@@ -50,6 +50,6 @@ Platformer.loadImages = function(src){
   for (var i = src.length - 1; i >= 0; i--) {
     Platformer.images[i] = new Image();
     Platformer.images[i].onload = start;
-    Platformer.images[i].src = src[i]
-  };
-}
+    Platformer.images[i].src = src[i];
+  }
+};
