f92f5dc37e1f23912b83eb167c4c0202f074fc01
Benjamin Landers <benjaminrlanders@gmail.com>
2013-12-24 18:48:47 -0800

    Documented collision

--- src/collision.js
+++ src/collision.js
@@ -1,41 +1,65 @@
-/**Tests if bottom of a sprite collides with with a group of other sprites**/
-Platformer.onGround = function(sprite, children){
-	return collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*0.2,sprite.getY()+sprite.getHeight(), children)||
-	collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*0.8,sprite.getY()+sprite.getHeight(), children);
+/**
+ * Constructor for collision
+ * @constructor
+ */
+Platformer.Collision = function(){
+  
 };
-/**Tests if the top of a sprite collides with a group of other sprites**/
-Platformer.collideHead = function(sprite, children){
-	return  collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*0.2,sprite.getY(), children)||
-	collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*0.8,sprite.getY(), children);
+/**
+ * Tests if bottom of a sprite collides with with a group of other sprites
+ * 
+ */
+Platformer.Collision.prototype.collideBottom = function(sprite, children,width){
+  width = width || 0.2;
+	return this.collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*(width),sprite.getY()+sprite.getHeight(), children)||
+	this.collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*(1-width),sprite.getY()+sprite.getHeight(), children);
 };
-/**Tests if the leftof a sprite collides with a group of other sprites**/
-Platformer.collideLeft = function(sprite, children){
+/**
+ * Tests if the top of a sprite collides with a group of other sprites
+ */
+Platformer.Collision.prototype.collideTop = function(sprite, children){
+  width = width || 0.2;
+	return  this.collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*width,sprite.getY(), children)||
+	this.collide(sprite.getX() + sprite.getWidth()*sprite.getScaleX()*(1-width),sprite.getY(), children);
+};
+/**
+ * Tests if the leftof a sprite collides with a group of other sprites
+ */
+Platformer.Collision.prototype.collideLeft = function(sprite, children){
+  width = width || 0.2;
 	if(sprite.getScaleX() > 0)
-		return collide(sprite.getX(),sprite.getY()+sprite.getHeight()*0.8, children)||
-		collide(sprite.getX(),sprite.getY()+sprite.getHeight()*0.2, children);
+		return this.collide(sprite.getX(),sprite.getY()+sprite.getHeight()*(1-width), children)||
+		this.collide(sprite.getX(),sprite.getY()+sprite.getHeight()*width, children);
 	else
-		return collide(sprite.getX() - sprite.getWidth(),sprite.getY()+sprite.getHeight()*0.8, children)||
-		collide(sprite.getX() - sprite.getWidth(),sprite.getY()+sprite.getHeight()*0.2, children);
+		return this.collide(sprite.getX() - sprite.getWidth(),sprite.getY()+sprite.getHeight()*(1-width), children)||
+		this.collide(sprite.getX() - sprite.getWidth(),sprite.getY()+sprite.getHeight()*width, children);
 };
-/**Tests if the Right of a sprite collides with a group of other sprites**/
-Platformer.collideRight = function(sprite, children){
+/**
+ * Tests if the Right of a sprite collides with a group of other sprites
+ */
+Platformer.Collision.prototype.collideRight = function(sprite, children){
+  width = width || 0.2;
 	if(sprite.getScaleX() > 0)
-		return collide(sprite.getX() + sprite.getWidth(),sprite.getY()+sprite.getHeight()*0.8, children)||
-		collide(sprite.getX() + sprite.getWidth(),sprite.getY()+sprite.getHeight()*0.2, children);
+		return this.collide(sprite.getX() + sprite.getWidth(),sprite.getY()+sprite.getHeight()*(1-width), children)||
+		this.collide(sprite.getX() + sprite.getWidth(),sprite.getY()+sprite.getHeight()*width, children);
 	else
-		return collide(sprite.getX(),sprite.getY()+sprite.getHeight()*0.8, children)||
-		collide(sprite.getX(),sprite.getY()+sprite.getHeight()*0.2, children);
+		return this.collide(sprite.getX(),sprite.getY()+sprite.getHeight()*(1-width), children)||
+		this.collide(sprite.getX(),sprite.getY()+sprite.getHeight()*width, children);
 };
-/**Tests if point intersects group of sprites**/
-Platformer.collide = function(x,y,children){
+/**
+ * Tests if point intersects group of sprites
+ */
+Platformer.Collision.prototype.collide = function(x,y,children){
 	for(var i = 0; i < children.length; i++){
-		if(testCollision(children[i],x,y))
+		if(this.testCollision(children[i],x,y))
 			return true;
 	}
 	return false;
 };
-/**Tests if point intersects a sprite**/
-Platformer.testCollision = function(object, x, y){
+/**
+ * Tests if point intersects a sprite
+ */
+Platformer.Collision.prototype.testCollision = function(object, x, y){
 	if(object.getY() > y || object.getY() + object.getHeight() < y)
 		return false;	
 	if(object.getScaleX() > 0){
