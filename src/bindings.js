ee409e7ae4d893c1bf41eb52043ba0dc77829175
Benjamin Landers <benjaminrlanders@gmail.com>
2013-12-24 18:09:45 -0800

    Added documentation

--- src/bindings.js
+++ src/bindings.js
@@ -1,64 +1,96 @@
-/**Makes bindings occur no matter what**/
-Platformer.setUnlockKeys = function(func){
-	Platformer.unlockKey = func;
+/**
+ * Constructor for Binding
+ * @constructor 
+ */
+Platformer.Binding = function(){
 };
-/**Stops bindings from occurring unless UnlockKeys is true**/
-Platformer.setBlockKeys = function(func){
-	Platformer.blockKey = func;
+/** 
+ * Sets overide for blockKey
+ * @method
+ * @param {Function} unlockKeys overide blockKeys and force bindings to be called
+ */
+Platformer.Binding.prototype.setUnlockKeys = function(unlockKey){
+	this.unlockKey = unlockKey;
+};
+/**
+ * Stops bindings from occurring unless UnlockKeys is true
+ * @param {Function} blockKeys block bindings from being called; instead have them be stored in keysBuffer
+ */
+Platformer.Binding.prototype.setBlockKeys = function(blockKey){
+	this.blockKey = blockKey ;
 };
 
-Platformer.bindingsDown = [];
-Platformer.bindingsUp = [];
-Platformer.keyBuffer = [];
-Platformer.keys = [];
-/**Sets a callback for a keycode on down**/
-Platformer.setBindingDown = function(key, func){
-	Platformer.bindingsDown[key]=func;
+Platformer.Binding.prototype.keyBuffer = [];
+Platformer.Binding.prototype.keys = [];
+/**
+ * Sets a callback for a keycode on down
+ * @param {Number} keycode Key to have the binding set to
+ * @param {Function} callback Called on keydown
+ */
+Platformer.Binding.prototype.setBindingDown = function(key, func){
+	this.bindingsDown[key]=func;
 };
-/**Sets a callback for a keycode on down**/
-Platformer.setBindingUp = function(key, func){
-	Platformer.bindingsDown[key]=func;
+/**
+ * Sets a callback for a keycode on up
+ * @param {Number} keycode Key to have the binding set to
+ * @param {Function} callback Called on keyup
+ */
+Platformer.Binding.prototype.setBindingUp = function(key, func){
+	this.bindingsDown[key]=func;
 };
-/**Moves a callback from one keycode to another**/
-Platformer.moveBindingUp = function(from, to){
-	Platformer.setBindingUp(to,Platformer.bindingsUp[from]);
-	Platformer.setBindingUp(from,function(){});
+/**
+ * Moves a callback from one keycode to another
+ * @param {Number} from source
+ * @param {Number} to destination
+ */
+Platformer.Binding.prototype.moveBindingUp = function(from, to){
+	this.setBindingUp(to,this.bindingsUp[from]);
+	this.setBindingUp(from,function(){});
 };
-/**Moves a callback from one keycode to another**/
-Platformer.moveBindingDown = function(from, to){
-	Platformer.setBindingDown(to,Platformer.bindingsDown[from]);
-	Platformer.setBindingDown(from,function(){});
+/**
+ * Moves a callback from one keycode to another
+ * @param {Number} from source
+ * @param {Number} to destination
+ */
+Platformer.Binding.prototype.moveBindingDown = function(from, to){
+	this.setBindingDown(to,this.bindingsDown[from]);
+	this.setBindingDown(from,function(){});
 };
-/**Initializes keybindings. Resets block and unlock keys and onkeydown and onkeyup**/
-Platformer.initKeys = function(up,down){
-	Platformer.bindingsDown = down || Platformer.bindingsDown || [];//uses input, current value or []
-	Platformer.bindingsUp = up || Platformer.bindingsUp || []; 
-	Platformer.keyBuffer = [];
-	Platformer.keys = [];
-	Platformer.blockKeys = Platformer.blockKeys || function(){ //stick with current value or function(){return false}
+/**
+ * Initializes keybindings.
+ * @param {Array} [up] array representing bindings for keyup
+ * @param {Array} [down] array representing bindings for keydown
+ */
+Platformer.Binding.prototype.initKeys = function(up,down){
+	this.bindingsDown = down || this.bindingsDown || [];//uses input, current value or []
+	this.bindingsUp = up || this.bindingsUp || []; 
+	this.keyBuffer = [];
+	this.keys = [];
+	this.blockKeys = this.blockKeys || function(){ //stick with current value or function(){return false}
 		return false;
 	};
-	Platformer.unlockKeys = Platformer.unlockKeys || function(){
+	this.unlockKeys = this.unlockKeys || function(){
 		return false;
 	};
+	var self = this
 	document.onkeydown = function(evt){
-    if(!Platformer.keys[evt.keyCode]){
-      Platformer.keys[evt.keyCode] = true;
+    if(!self.keys[evt.keyCode]){
+      self.keys[evt.keyCode] = true;
       console.log(evt.keyCode); //logs keyCode
-      if(!Platformer.blockKeys(evt) || Platformer.unlockKeys(evt)){
-        if(Platformer.bindingsDown[evt.keyCode])
-          Platformer.bindingsDown[evt.keyCode](evt);
+      if(!self.blockKeys(evt) || self.unlockKeys(evt)){
+        if(self.bindingsDown[evt.keyCode])
+          self.bindingsDown[evt.keyCode](evt);
       }else
-      Platformer.keyBuffer.push(evt); //adds evt to be processed later
+      self.keyBuffer.push(evt); //adds evt to be processed later
     }
 	};
 	document.onkeyup = function(evt){
-    Platformer.keys[evt.keyCode] = false;
-    if(!Platformer.blockKeys(evt) || Platformer.unlockKeys(evt)){
-      if(Platformer.bindingsUp[evt.keyCode])
-      Platformer.bindingsUp[evt.keyCode](evt);
+    self.keys[evt.keyCode] = false;
+    if(!self.blockKeys(evt) || self.unlockKeys(evt)){
+      if(self.bindingsUp[evt.keyCode])
+      self.bindingsUp[evt.keyCode](evt);
 		}else{
-			Platformer.keyBuffer.push(evt); //adds evt to be processed later
+			self.keyBuffer.push(evt); //adds evt to be processed later
 		}
 	};
 };
